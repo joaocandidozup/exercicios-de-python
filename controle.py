7b0dfa6b8c58b00f83852cb3a02b5a0641e434db
joaocandidozup <joao.candido@zup.com>
2025-04-16 13:51:46 -0300

    feat:Add usage examples and display wallet information

--- controle.py
+++ controle.py
@@ -65,3 +65,23 @@ class Carteira:
         print(f"Saldo final: R$ {self.saldo():.2f}")
 
 
+carteira = Carteira()
+
+transacao1 = Transacao("Salário", 2500, "Renda", "1/04/2025")
+transacao2 = Transacao("Mercado", -300, "Alimentação", "30/04/2025")
+transacao3 = Transacao("Aluguel", -1200, "Moradia", "1/04/2025")
+transacao4 = Transacao("Extra", 100, "Renda", "15/04/2025")
+transacao5 = Transacao("Restaurante", -50, "Alimentação", "6/04/2025")
+
+
+carteira.adicionar(transacao1)
+carteira.adicionar(transacao2)
+carteira.adicionar(transacao3)
+carteira.adicionar(transacao4)
+carteira.adicionar(transacao5)
+
+carteira.exibir_transacoes()
+
+carteira.filtrar_por_categoria("Alimentação")
+
+carteira.resumo_geral()
