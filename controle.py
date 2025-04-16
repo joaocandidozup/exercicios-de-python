f5e568c8be4a2e4f44acc2166473c026f417c831
joaocandidozup <joao.candido@zup.com>
2025-04-16 11:48:46 -0300

    feat: add transaction class with attributes and method

--- controle.py
+++ controle.py
@@ -1,1 +1,15 @@
 
+class Transacao:
+    def __init__(self, descricao, valor, categoria, data):
+        self.descricao = descricao
+        self.valor = valor
+        self.categoria = categoria
+        self.data = data
+
+    def resumo(self):
+        if self.valor > 0:
+            sinal = "+"
+        else:
+           sinal = "-"
+        return f"{self.descricao} | {sinal}{abs(self.valor)} | {self.categoria} | {self.data}"
+
