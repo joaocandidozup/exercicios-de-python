7f25d5866ec0a98575b0d5515495ff210110cd3f
joaocandidozup <joao.candido@zup.com>
2025-04-22 14:21:44 -0300

    feat: adiciona as classes concretas FuncionarioComum e Gerente

--- bonus-funcionario/funcionarios.py
+++ bonus-funcionario/funcionarios.py
@@ -23,3 +23,14 @@ class Funcionario(ABC):
     def calcular_bonus(self) -> float:
         pass
 
+class FuncionarioComum(Funcionario):
+    def calcular_bonus(self) -> float:
+        return self.salario * 0.10
+
+class Gerente(Funcionario):
+    def __init__(self, nome: str, salario: float, bonus_adicional: float):
+        super().__init__(nome, salario)
+        self.bonus_adicional = bonus_adicional
+
+    def calcular_bonus(self) -> float:
+        return self.salario * 0.20 + self.bonus_adicional
